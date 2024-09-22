a52ad3c87f46b812d496e43c278414d0311b22cb
oweeeenie <owensullivan419@yahoo.com>
2024-09-22 02:38:21 -0400

    created Gameboard functionality, ready to start on player/controller functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+const Gameboard = (function () {
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  function getBoard() {
+    return board;
+  }
+  return {
+    gameboardStatus: getBoard,
+  };
+
+  function placeMark(index, mark) {
+    if (index <= 8 && index >= 0) {
+      if (board[index] === "") {
+        board[index] = mark;
+        return true;
+      } else {
+        console.log("This spot is already being used!");
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+})();
